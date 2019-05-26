789ec0de0a4330f8de6747042de2263bf6595d74
bozeiza <bozeiza@gmail.com>
2019-05-26 23:17:45 +0200

    changed map image, set new camera position

--- app/src/main/java/com/zpi/xmas2019/MarketMapsActivity.kt
+++ app/src/main/java/com/zpi/xmas2019/MarketMapsActivity.kt
@@ -50,14 +50,11 @@ class MarketMapsActivity : AppCompatActivity(), OnMapReadyCallback, StallDetails
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_market_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         stalls.addAll(DummyStalls.STALLS)
-
-
 
         val sheetBehavior = BottomSheetBehavior.from<NestedScrollView>(bottom_sheet)
 
@@ -133,7 +130,7 @@ class MarketMapsActivity : AppCompatActivity(), OnMapReadyCallback, StallDetails
         }
         if(item.itemId != R.id.filter) {
             mMap.clear()
-            item.setChecked(!item.isChecked())
+            item.isChecked= !item.isChecked
             when (item.getItemId()) {
                 R.id.show_all -> {
 
@@ -150,7 +147,6 @@ class MarketMapsActivity : AppCompatActivity(), OnMapReadyCallback, StallDetails
                     return true
                 }
                 R.id.show_food -> {
-                    System.out.println("food chosen")
                     if (item.isChecked()) {
                         showFood = true
                         showAll = false
@@ -195,43 +191,24 @@ class MarketMapsActivity : AppCompatActivity(), OnMapReadyCallback, StallDetails
         return true
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
     fun dummyFilter(){
         searchTags("cz")
         searchTags("pierniki")
 
         with(mMap){
             if(showArt or showAll){
-//                addMarker(MarkerOptions().position(LatLng(51.108962, 17.033640)).title("Stoisko 103"))
-//                addMarker(MarkerOptions().position(LatLng(51.109997, 17.03330)).title("Stoisko 29")  )
                 val filteredStalls = stalls.filter { it.tags.contains("rękawiczki") ||  it.tags.contains("lampki")} as ArrayList<DummyStalls.Stall>
                 createMarkers(filteredStalls).forEach { addMarker(it) }
             }
             if (showFood or showAll){
-//                addMarker(MarkerOptions().position(LatLng(51.108518, 17.032859)).title("Stoisko 121"))
-//                addMarker(MarkerOptions().position(LatLng(51.110317, 17.033450)).title("Stoisko 129")  )
                 val filteredStalls = stalls.filter { it.tags.contains("sery") ||  it.tags.contains("pierniki") ||  it.tags.contains("pierogi") || it.tags.contains("wino")} as ArrayList<DummyStalls.Stall>
                 createMarkers(filteredStalls).forEach { addMarker(it) }
             }
             if (showBeauty or showAll){
-//                addMarker(MarkerOptions().position(LatLng(51.109235, 17.032358)).title("Stoisko 124"))
-//                addMarker(MarkerOptions().position(LatLng(51.109235, 17.032937)).title("Stoisko 105")  )
                 val filteredStalls = stalls.filter { it.tags.contains("czapki") ||  it.tags.contains("rękawiczki")} as ArrayList<DummyStalls.Stall>
                 createMarkers(filteredStalls).forEach { addMarker(it) }
             }
             if(showXmas or showAll){
-//                addMarker(MarkerOptions().position(LatLng(51.109422, 17.031519)).title("Stoisko 124"))
-//                addMarker(MarkerOptions().position(LatLng( 51.109563, 17.030724)).title("Stoisko 105")  )
-//                addMarker(MarkerOptions().position(LatLng(  51.109563, 17.030724)).title("Stoisko 124"))
                 val filteredStalls = stalls.filter { it.tags.contains("rękawiczki") ||  it.tags.contains("lampki") || it.tags.contains("czapki")} as ArrayList<DummyStalls.Stall>
                 createMarkers(filteredStalls).forEach { addMarker(it) }
             }
@@ -244,18 +221,16 @@ class MarketMapsActivity : AppCompatActivity(), OnMapReadyCallback, StallDetails
 
     }
     fun setUpMap(){
-        val xMarketPosition = LatLng(51.109885, 17.032344)
-//        mMap.addMarker(MarkerOptions().position(xMarketPosition).title("Jarmark"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(xMarketPosition))
-        val area_bounds: LatLngBounds= LatLngBounds(LatLng(51.107337, 17.027925), LatLng(51.112358, 17.036697))
-        with(mMap){addGroundOverlay(GroundOverlayOptions().apply {
-            image(BitmapDescriptorFactory.fromResource(R.drawable.mapa2222))
-            positionFromBounds(area_bounds)
-            //transparency(0.5f)
-
+        val cameraPosition = LatLng(51.1099955, 17.0323775)
+        val areaBounds = LatLngBounds(LatLng(51.107022, 17.027541), LatLng(51.112969, 17.037214))
+        with(mMap){
+            moveCamera(CameraUpdateFactory.newLatLng(cameraPosition))
+            addGroundOverlay(GroundOverlayOptions().apply {
+            image(BitmapDescriptorFactory.fromResource(R.drawable.ic_framedmap))
+            positionFromBounds(areaBounds)
             clickable(true)
         })
-            setLatLngBoundsForCameraTarget(LatLngBounds(LatLng(51.1078, 17.028),LatLng(51.112, 17.0355)))
+            setLatLngBoundsForCameraTarget(LatLngBounds(LatLng(51.1088, 17.0296),LatLng(51.111, 17.035)))
             setMinZoomPreference(17f)
             isMyLocationEnabled = true
             uiSettings.setAllGesturesEnabled(true)
